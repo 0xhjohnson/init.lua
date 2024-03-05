a874d91ed57836e3315234f35030339281ed0549
Hunter Johnson <hunter.johnson@goodrx.com>
2024-03-05 11:33:56 -0500

    fix: eslint formatting support

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -7,12 +7,27 @@ return {
 
 		return {
 			sources = {
-				nls.builtins.formatting.prettierd,
+				nls.builtins.formatting.eslint_d,
+				nls.builtins.formatting.prettierd.with({
+					filetypes = { 
+						"css",
+						"html",
+						"json",
+						"graphql",
+						"yaml",
+						"markdown",
+					},
+				}),
 				nls.builtins.formatting.goimports,
 				nls.builtins.diagnostics.vale,
 				nls.builtins.diagnostics.eslint_d,
 				nls.builtins.code_actions.eslint_d
 			},
+			on_attach = function(client)
+				if client.server_capabilities.document_formatting then
+					vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+				end
+			end,
 		}
 	end,
 }
