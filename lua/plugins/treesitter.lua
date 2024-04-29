01ed211fbdfc7aaf973a0563ad849635219d9ee9
Hunter Johnson <hunter.johnson@goodrx.com>
2024-04-29 15:15:08 -0400

    fix: treesitter error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,31 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = "BufReadPost",
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-		context_commentstring = { enable = true, enable_autocmd = false },
-		ensure_installed = {
-			"bash",
-			"css",
-			"dockerfile",
-			"go",
-			"help",
-			"html",
-			"javascript",
-			"json",
-			"latex",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"rust",
-			"sql",
-			"tsx",
-			"typescript",
-			"yaml",
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        context_commentstring = { enable = true, enable_autocmd = false },
+        ensure_installed = {
+            "bash",
+            "css",
+            "dockerfile",
+            "go",
+            "html",
+            "javascript",
+            "json",
+            "latex",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "rust",
+            "sql",
+            "tsx",
+            "typescript",
+            "yaml",
+        },
+        sync_install = false
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end
 }
